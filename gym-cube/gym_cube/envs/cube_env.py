96d901b650c49b34b486c326e0415df93e1786b0
soochul <usam4760@gmail.com>
2022-10-11 00:55:25 +0900

    1st commit

--- gym-cube/gym_cube/envs/cube_env.py
+++ gym-cube/gym_cube/envs/cube_env.py
@@ -3,22 +3,26 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from gym_cube.envs.assets.cube import Cube 
+from gym_cube.envs.assets.cube_interactive import InteractiveCube, Cube
 
 from collections import namedtuple
 from gym.wrappers.monitoring.video_recorder import VideoRecorder
 
+'''
+UDFBRLU'D'F'B'R'L'
+01234567891011
+action = {'U':0}
 
 
+'''
 class CubeEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, cube_size = 3):
         self.cube = Cube(cube_size)
+        self.icube = InteractiveCube(cube=self.cube)
         self.N = cube_size
-        self.state = self.cube.stickers
-        self.done = 1
-        self.reward = 0
+        self.state = [self.cube._face_centroids, self.cube._faces, self.cube._sticker_centroids, self.cube._stickers]
         self.num_turns = 0
 
         self.transaction = namedtuple('Point', ['state', 'target_value', 'target_policy', 'scramble_count'])
@@ -26,47 +30,52 @@ class CubeEnv(gym.Env):
 
     def step(self, action):
         face, layer, degree = action
-        self.cube.move(face, layer, degree)
+        # self.cube.move(face, layer, degree)
+        self.cube.rotate_face(face, degree, layer)
         self.num_turns += 1
         # check done
-        for i in range(len(self.state)):
-            side = self.state[i]
-            if side[0][0] == (np.sum(side) / (self.N * self.N)): 
-                self.done *= 1
-            else:
-                self.done *= 0
-        # reward shaping
-        if self.done == 1:
-            self.reward += 1
-        else:
-            self.reward -= 1
-        return self.state, self.reward, self.done
+        done = 1
+        reward = 0
+        # for i in range(len(self.state)):
+        #     side = self.state[i]
+        #     if side[0][0] == (np.sum(side) / (self.N * self.N)): 
+        #         done *= 1
+        #     else:
+        #         done *= 0
+        # # reward shaping
+        # if done == 1:
+        #     reward += 1
+        # else:
+        #     reward -= 1
+        return self.state, reward, done
     
     def reset(self):
-        self.state = np.array([np.tile(i, (self.N, self.N)) for i in range(6)])
-        self.done = 0
-        self.reward = 0
+        # self.state = np.array([np.tile(i, (self.N, self.N)) for i in range(6)])
+        self.cube._initialize_arrays()
         self.num_turns = 0
-        return self.state
+        return self.state, self.num_turns
+
     
     def randomize(self, seed = None ,scramble_count=1000):
-        for t in range(scramble_count):
-            face = self.cube.dictface[np.random.randint(6)]
-            layer = np.random.randint(self.N)
-            degree = 1 if np.random.random() < 0.5 else -1
-            self.cube.move(face, layer, degree)
-        self.render((0, 1))
+        # for t in range(scramble_count):
+        #     face = self.cube.dictface[np.random.randint(6)]
+        #     layer = np.random.randint(self.N)
+        #     degree = 1 if np.random.random() < 0.5 else -1
+        #     self.cube.move(face, layer, degree)
+        # self.render_mode((0, 1))
+
+        self.icube._random_view(steps = scramble_count)
 
     def render_mode(self, mode):
-        flat = mode[0]
-        views = mode[1]
-        self.cube.render(flat, views)
-        plt.show()
+        # flat = mode[0]
+        # views = mode[1]
+        # self.cube.render(flat, views)
+        # self.icube._draw_cube()
+        # plt.show()
+        pass
     
     def render(self):
-        flat = False
-        views = True
-        self.cube.render(flat, views)
+        fig = self.cube.draw_interactive()
         plt.show()
 
     def save_video(self, env, policy, video_path):
@@ -75,6 +84,7 @@ class CubeEnv(gym.Env):
         num_actions = len(policy)
         i = 0
         while i < num_actions:
+            print(i)
             recorder.capture_frame()
             action = policy[i]
             state, reward, done = env.step(action)
--- gym-cube/test.py
+++ gym-cube/test.py
@@ -5,12 +5,18 @@ import time
 
 e = gym.make("cube-v0")
 e.reset()
-ts = time.time()
-# action = ("U", 1, 1)
-# e.step(action)
+# ts = time.time()
+action = ("U", 1, -1)
+# e.randomize(seed = None, scramble_count= 60000) # 13.37 sec
 # mode = [0, 1]
 # e.render_mode(mode)
-policy = [("U", 1, 1), ("U", 1, 1), ("U", 1, 1), ("U", 1, 1)]
-path = 'gym-cube/result'
-e.save_video(e, policy, path)
-print(time.time() - ts)
+# e.step(action)
+e.randomize(scramble_count=10)
+e.reset()
+e.render()
+
+
+# policy = [("U", 1, 1), ("U", 1, 1), ("U", 1, 1), ("U", 1, 1)]
+# path = 'gym-cube/result'
+# e.save_video(e, policy, path)
+# print(time.time() - ts)
