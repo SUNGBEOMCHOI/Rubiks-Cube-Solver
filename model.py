553ef823046af002d9292f77154a59ac51594406
leepk6282 <leepk6282@yonsei.ac.kr>
2022-10-10 14:09:18 +0900

    Update model.py

--- model.py
+++ model.py
@@ -3,25 +3,41 @@ import torch.nn as nn
 
 class DeepCube(nn.Module):
     def __init__(self, state_dim, action_dim):
-        super().__init__()
-        self.encoder_net = nn.Sequential()
-        self.policy_net = nn.Sequential()
-        self.value_net = nn.Sequential()
+        super(DeepCube, self).__init__()
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.encoder_net = nn.Sequential(
+            nn.Flatten(start_dim = 0, end_dim = -1),
+            nn.Linear(20*24,  4096),
+            nn.ELU(),
+            nn.Linear(4096, 2048),
+            nn.ELU()
+        )
+        self.policy_net = nn.Sequential(
+            nn.Linear(2048, 512),
+            nn.ELU(),
+            nn.Linear(512, 12)
+        )
+        self.value_net = nn.Sequential(
+            nn.Linear(2048, 512),
+            nn.ELU(),
+            nn.Linear(512, 1)
+        )
 
     def forward(self, x):
         """
-        Return action probability and state values corresponding input states
+        Return action output and state values corresponding input states
 
         Args:
             x: input state of size [batch_size, state_dim[0], state_dim[1]]
         Returns:
             value: Torch tensor of state value of size [batch_size, 1]
-            action_probs: Torch tensor of action probability of size [batch_size, action_dim]
+            action_output: Torch tensor of action probability of size [batch_size, action_dim]
         """
         x = self.encoder_net(x)
         value = self.value_net(x)
-        action_probs = self.policy_net(x)
-        return value, action_probs
+        action_output = self.policy_net(x)
+        return value, action_output
 
     def get_action(self, x):
         """
@@ -32,4 +48,5 @@ class DeepCube(nn.Module):
         Returns:
             action: Integer of action
         """
-        pass
+        action_output = forward(x)[1]
+        return torch.argmax(action_output)
