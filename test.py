84a5aaa7531e70ac46193315c376901f9c114348
leepk6282 <leepk6282@yonsei.ac.kr>
2022-11-03 21:26:36 +0900

    mask for test

--- test.py
+++ test.py
@@ -20,16 +20,16 @@ def test(cfg, mode = 'show'):
         cfg: Which contains trial configuration
         mode: show(directly pop up video) or save(save test result graphs)
     """
-    
+
+    hidden_dim = cfg['model']['hidden_dim']
     device = torch.device('cuda' if cfg['device']=='cuda' and torch.cuda.is_available() else 'cpu')
     cube_size = 2 # cfg['test]['cube_size']
     state_dim, action_dim = get_env_config(cube_size)
-    deepcube = DeepCube(state_dim, action_dim).to(device) # load model
-    env = make_env(cube_size) # make environment # 여기까지 0.08초
+    deepcube = DeepCube(state_dim, action_dim, hidden_dim).to(device) # load model
+    env = make_env(device, cube_size) # make environment
 
-    #pretrained model 위치 넣으면 됨
-    deepcube.load_state_dict(torch.load('model_2600.pt', map_location = device)['model_state_dict'])
-
+    test_model_path = '/test/model'
+    os.makedirs(test_model_path, exist_ok = True)
 
     if mode == 'save':
         time1 = time.time()
@@ -40,14 +40,15 @@ def test(cfg, mode = 'show'):
     elif mode == 'show': # 화면에 띄우기
         scramble_count = 100
         seed = None
-        trial(deepcube, env, cfg, scramble_count, seed)
+        deepcube.load_state_dict(torch.load(test_model_path + '\\7500.pt', map_location = device)['model_state_dict'])
+        trial(deepcube, env, cfg, scramble_count, seed, mask=True, save=False)
         # trial 결과 나왔으니 시뮬레이션 띄우기 
         pass
     else : # show랑 save 둘다 아님
         print('You can only choose \"show\" or \"save\" as mode factor')
         raise ValueError
 
-def trial(model, env, cfg, scramble_count, seed = None):
+def trial(model, env, cfg, scramble_count, seed = None, mask=True, save = True):
     """
     Try to solve a cube with given state
     Args:
@@ -61,14 +62,21 @@ def trial(model, env, cfg, scramble_count, seed = None):
         solve_time: time to solve a cube (sec)
         trial_result: True or False
     """
-    max_timesteps = 1000
+    max_timesteps = 200 # max_timesteps 1000이랑 30이랑 정확하게 동일함
     solve_count, solve_time, trial_result = 0, 0, 0
-    state, done = env.reset(seed, scramble_count), False
+    state, done, pre_action = env.reset(seed, scramble_count), False, None
     start_time = time.time()
+    action_list=[]
     for timestep in range(1, max_timesteps+1):
         with torch.no_grad():
             state_tensor = torch.tensor(state).float().detach()
-            action = model.get_action(state_tensor)
+            if mask:
+                action = model.get_action(state_tensor, pre_action)
+                if save==False:
+                    action_list.append(action)
+                pre_action = action
+            else:
+                action = model.get_action(state_tensor)
         next_state, _, done, _ = env.step(action)
         if done:
             solve_count = timestep
@@ -76,12 +84,15 @@ def trial(model, env, cfg, scramble_count, seed = None):
             trial_result = 1
             break
         state = next_state
+        
         if timestep == max_timesteps:
             solve_count = None
             solve_time = time.time() - start_time
+    if save==False:
+        print(action_list)
     return solve_count, solve_time, trial_result
 
-def plot_test_solve_percentage(model, env, cfg):
+def plot_test_solve_percentage(model, env, cfg, save_file_path = './test', test_model_path = './test/model'):
     """
     Make plot which shows solve percentage by scramble counts
     Args:
@@ -89,38 +100,48 @@ def plot_test_solve_percentage(model, env, cfg):
         env: Cube environment
         cfg: Which contains trial configuration
     """
-    save_file_path = './video'
     os.makedirs(save_file_path, exist_ok = True)
+    os.makedirs(test_model_path, exist_ok = True)
+    device = 'cpu'
     trial_scramble_count = 30
     trial_cube_count = 50
+    model_list = os.listdir(test_model_path)
 
-    solve_count_table = np.zeros((trial_scramble_count, trial_cube_count))
-    solve_time_table = np.zeros((trial_scramble_count, trial_cube_count))
-    trial_result_table = np.zeros((trial_scramble_count, trial_cube_count))
-    
-    for scramble_count in range(1, trial_scramble_count+1): # 움직인 횟수 별로
-        for cube_count in range(trial_cube_count): # 1,2,3,,, 50개의 큐브
-            seed = cube_count
-            solve_count, solve_time, trial_result = trial(model, env, cfg, scramble_count, seed)
+    fig, ax = plt.subplots(figsize = (6,6), facecolor = '#c1f1f1')
+
+    for model_name in model_list:
+        model.load_state_dict(torch.load(test_model_path + '/' + model_name, map_location = device)['model_state_dict'])
+
+        solve_count_table = np.zeros((trial_scramble_count, trial_cube_count))
+        solve_time_table = np.zeros((trial_scramble_count, trial_cube_count))
+        trial_result_table = np.zeros((trial_scramble_count, trial_cube_count))
             
-            solve_count_table[scramble_count-1][cube_count] = solve_count
-            solve_time_table[scramble_count-1][cube_count] = solve_time
-            trial_result_table[scramble_count-1][cube_count] = trial_result
+        for mask in [True, False]:
+            for scramble_count in range(1, trial_scramble_count+1): # 움직인 횟수 별로
+                for cube_count in range(trial_cube_count): # 1,2,3,,, 50개의 큐브
+                    seed = cube_count
+                    solve_count, solve_time, trial_result = trial(model, env, cfg, scramble_count, seed, mask=mask)
+                    
+                    solve_count_table[scramble_count-1][cube_count] = solve_count
+                    solve_time_table[scramble_count-1][cube_count] = solve_time
+                    trial_result_table[scramble_count-1][cube_count] = trial_result
 
-    plt.figure(figsize = (6,6))
-    scramble_distance_list = [i for i in range(1, trial_scramble_count+1)]
-    solve_count_list = [trial_result_table[scramble_count-1].mean() * 100 for scramble_count in range(1, trial_scramble_count+1)]
-    plt.plot(scramble_distance_list, solve_count_list, 'r--')
-    plt.title('Difficulty vs Solve Percentage')
-    plt.xlabel('Scramble Distance')
-    plt.ylabel('Percentage solved')
-    plt.legend()
-    plt.xticks(np.linspace(1, 31, 7, endpoint = True))
-    plt.yticks(np.linspace(0, 100, 6, endpoint = True))
+            
+            scramble_distance_list = [i for i in range(1, trial_scramble_count+1)]
+            solve_count_list = [trial_result_table[scramble_count-1].mean() * 100 for scramble_count in range(1, trial_scramble_count+1)]
+            ax.plot(scramble_distance_list, solve_count_list, '--', label = f'{"Mask"*mask} {model_name[:-3]}')
+            # print(solve_count_list, len(solve_count_list), sep='\n')
+
+    ax.set_title('Difficulty vs Solve Percentage')
+    ax.set_xlabel('Scramble Distance')
+    ax.set_ylabel('Percentage solved')
+    ax.legend(loc = 'upper right')
+    ax.set_xticks(np.linspace(1, 31, 7, endpoint = True))
+    ax.set_yticks(np.linspace(0, 100, 6, endpoint = True))
     plt.savefig(f'{save_file_path}/Difficulty vs Solve Percentage.png', dpi = 300)
-    # plt.show()
+    plt.show()
 
-def plot_test_distribution_and_dispersion(model, env, cfg):
+def plot_test_distribution_and_dispersion(model, env, cfg, save_file_path = './test', test_model_path = './test/model'):
     """
     Make histogram which shows distribution of solve times and boxplot which shows dispersion of scramble counts 
     Args:
@@ -128,43 +149,57 @@ def plot_test_distribution_and_dispersion(model, env, cfg):
         env: Cube environment
         cfg: Which contains trial configuration
     """
-    save_file_path = './video'
+
     os.makedirs(save_file_path, exist_ok = True)
+    os.makedirs(test_model_path, exist_ok = True)
+    device = 'cpu'
     trial_scramble_count = 1000
     trial_cube_count = 640
+    model_list = os.listdir(test_model_path)
 
-    solve_count_table = np.zeros(trial_cube_count)
-    solve_time_table = np.zeros(trial_cube_count)
-    trial_result_table = np.zeros(trial_cube_count)
-    
-    for cube_count in range(trial_cube_count): # 1,2,3,,, 640개의 큐브
-        seed = cube_count
-        solve_count, solve_time, trial_result = trial(model, env, cfg, trial_scramble_count, seed)
+    fig, axes = plt.subplots(2,1, figsize = (6,12), facecolor = '#c1f1f1')
+    axes[0].set_title('Distribution of Solve Time')
+    axes[0].set_xlabel('Seconds')
+    axes[0].set_ylabel('Number of cubes')
+    axes[1].set_title('Dispersion of Scramble Counts')
+    axes[1].set_xlabel('Model')
+    axes[1].set_ylabel('Number of moves')
+    # axes[1].set_ylim(0,25)
+
+    for idx1, model_name in enumerate(model_list):
+        model.load_state_dict(torch.load(test_model_path + '\\' + model_name, map_location = device)['model_state_dict'])
+
+        solve_count_table = np.zeros(trial_cube_count)
+        solve_time_table = np.zeros(trial_cube_count)
+        trial_result_table = np.zeros(trial_cube_count)
         
-        solve_count_table[cube_count] = solve_count
-        solve_time_table[cube_count] = solve_time
-        trial_result_table[cube_count] = trial_result
+        for idx2, mask in enumerate([True, False]):
+            for cube_count in range(trial_cube_count): # 1,2,3,,, 640개의 큐브
+                seed = cube_count
+                solve_count, solve_time, trial_result = trial(model, env, cfg, trial_scramble_count, seed, mask=mask)
+                
+                solve_count_table[cube_count] = solve_count
+                solve_time_table[cube_count] = solve_time
+                trial_result_table[cube_count] = trial_result
+            
+            solve_time_list_s = solve_time_table[~np.isnan(solve_count_table)]
+            solve_time_list_f = solve_time_table[np.isnan(solve_count_table)]
+            axes[0].hist(solve_time_list_s, range=(0, 0.04), bins=40, alpha = 0.9, label = f'{"Mask"*mask} {model_name[:-3]}', histtype='step')
+            # axes[0].hist([solve_time_list_s, solve_time_list_f], bins = 100, label = ['Success', 'Failure'], color = ['blue', 'red'])
+            axes[1].boxplot(solve_count_table[~np.isnan(solve_count_table)], positions=range(idx1*2+idx2+1, idx1*2+idx2+2))
+            # print(solve_count_table[~np.isnan(solve_count_table)], solve_count_table[~np.isnan(solve_count_table)].shape, sep= '\n')
     
-    solve_time_list_s = solve_time_table[~np.isnan(solve_count_table)]
-    solve_time_list_f = solve_time_table[np.isnan(solve_count_table)]
-    plt.figure(figsize = (6,6))
-    plt.axvline(solve_time_list_s.mean(), color = 'grey', linestyle = '--', linewidth = 0.5)
-    plt.hist([solve_time_list_s, solve_time_list_f], bins = 100, label = ['Success', 'Failure'], color = ['blue', 'red'])
-    plt.title('Distribution of Solve Time')
-    plt.xlabel('Seconds')
-    plt.ylabel('Number of cubes')
-    plt.legend()
-    # plt.xticks(np.linspace(0, 10, 6, endpoint = True))
-    # plt.yticks(np.linspace(0, 100, 5, endpoint = True))
-    plt.savefig(f'{save_file_path}/Distribution of Solve Time.png', dpi = 300)
-    # plt.show()
+        axes[0].legend(loc = 'upper right')
+        axes[1].set_xticks([idx for idx in range(1, (idx2+1)*len(model_list)+1)])
+        # axes[1].set_xticklabels([f'{"Mask"*mask} {model_name[:-3]}' for model_name in model_list])
+        axes[1].set_xticklabels(['Mask 2600', '2600', 'Mask 7500', '7500'])
 
-    plt.figure(figsize = (6,6))
-    plt.boxplot(solve_count_table[~np.isnan(solve_count_table)])
-    plt.title('Dispersion of Scramble Counts')
-    plt.xlabel('Model')
-    plt.ylabel('Number of moves')
-    plt.xticks([1], ['model1'])
+    # # plt.figure(figsize = (6,6))
+    # # plt.boxplot(solve_count_table[~np.isnan(solve_count_table)])
+    # plt.title('Dispersion of Scramble Counts')
+    # plt.xlabel('Model')
+    # plt.ylabel('Number of moves')
+    # plt.xticks([1], ['model1'])
     plt.savefig(f'{save_file_path}/Dispersion of scramble count.png', dpi = 300)
     # plt.show()
 
